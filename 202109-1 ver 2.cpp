9fd132138b0aeb36546fe4188566c7be69b63e88
liyeukhou <liyeukhou@mails.swust.edu.cn>
2021-11-16 18:52:12 +0800

    update logic of comparison and Format lines

--- 202109-1 ver 2.cpp
+++ 202109-1 ver 2.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-    int firstline=0;
-    cin>>firstline;
-    int array[100]={0};
-    int arraymax[100]={0};
-    int subscript=0;
+    int firstline = 0;
+    cin >> firstline;
+    int array[100] = {0};
+    int arraymax[100] = {0};
+    int subscript = 0;
     int theFirstNumber;
-    cin>>theFirstNumber;
-    array[subscript]=theFirstNumber;
-    arraymax[subscript]=theFirstNumber;
+    cin >> theFirstNumber;
+    array[subscript] = theFirstNumber;
+    arraymax[subscript] = theFirstNumber;
     subscript++;
     int waitForCompare;
-    for(int temp=0;temp<firstline-1;temp++)
-    {
-        cin>>waitForCompare;
-        if(waitForCompare>array[subscript-1])
-        {
-            array[subscript]=waitForCompare;
-            arraymax[subscript]=waitForCompare;
+    for (int temp = 0; temp < firstline - 1; temp++) {
+        cin >> waitForCompare;
+        if (waitForCompare > arraymax[subscript - 1]) {
+            array[subscript] = waitForCompare;
+            arraymax[subscript] = waitForCompare;
             subscript++;
             continue;
         }
-        if(waitForCompare=array[subscript-1])
-        {
+        if (waitForCompare = arraymax[subscript - 1]) {
             //min
-            array[subscript]=0;
+            array[subscript] = 0;
             //max
-            arraymax[subscript]=waitForCompare;
+            arraymax[subscript] = waitForCompare;
             subscript++;
             continue;
         }
     }
-    int min=0;
-    int max=0;
-    subscript=0;
-    for(int count=0;count<sizeof(array)/sizeof(array[0]);count++)
-    {
-        min= min + array[subscript];
+    int min = 0;
+    int max = 0;
+    subscript = 0;
+    for (int count = 0; count < sizeof(array) / sizeof(array[0]); count++) {
+        min = min + array[subscript];
         subscript++;
     }
-    subscript=0;
-    for(int count=0;count<sizeof(arraymax)/sizeof(array[0]);count++)
-    {
-        max=max+arraymax[subscript];
+    subscript = 0;
+    for (int count = 0; count < sizeof(arraymax) / sizeof(array[0]); count++) {
+        max = max + arraymax[subscript];
         subscript++;
     }
-    cout << max<<endl<<min;
+    cout << max << endl << min;
     return 0;
 }
